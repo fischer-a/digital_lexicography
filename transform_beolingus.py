15a949528570e735566e8b6610d457d0acf2563e
fischer-a <44682161+fischer-a@users.noreply.github.com>
2018-11-25 22:08:13 +0100

    Add files via upload

---
+++ transform_beolingus.py
@@ -0,0 +1,110 @@
+from lxml import etree
+import input_output
+import pprint
+import re
+
+
+def transform_in_tei(beo_as_dict):
+    root = etree.Element('TEI', attrib={"xmlns": 'http://www.tei-c.org/ns/1.0'})
+
+    ##HEADER
+    header = etree.SubElement(root, 'teiHeader')
+
+    fileDesc = etree.SubElement(header, 'fileDesc')
+    titleStmt = etree.SubElement(fileDesc, 'titleStmt')
+    title = etree.SubElement(titleStmt, 'title')
+    title.text = 'TEI Version of Beolingus DE-EN'
+
+    publicationStmt = etree.SubElement(fileDesc, 'publicationStmt')
+    p_publicationStmt = etree.SubElement(publicationStmt, 'p')
+    p_publicationStmt.text = 'Original Data: Copyright (c) :: Frank Richter <frank.richter.tu-chemnitz.de>'
+
+    sourceDesc = etree.SubElement(fileDesc, 'sourceDesc')
+    p_sourceDesc = etree.SubElement(sourceDesc, 'p')
+    p_sourceDesc.text = 'Digi_Lex - TEI Version'
+
+    ##TEXT
+    text = etree.SubElement(root, 'text')
+    body = etree.SubElement(text, 'body')
+
+    for k1, v1 in beo_as_dict.items():  ##k1 ist die Zeilenzahl, v1 der Eintrag pro Zeile im dict
+        superentry = etree.Element('superentry')
+        #print(k1)
+        for k2, v2 in v1.items():
+            split_forms = k2.split(';')
+            split_senses = v2.split(';')
+            '''
+            print("k:")
+            pprint.pprint(split_forms)
+            print(len(split_forms))
+            print("v:")
+            pprint.pprint(split_senses)
+            print(len(split_senses))
+            '''
+            entry = etree.SubElement(superentry, 'entry')
+
+            for f in split_forms:
+                f = f.lstrip()
+                form = etree.SubElement(entry, 'form')
+                orth = etree.SubElement(form, 'orth')
+                #orth_pattern = re.compile(r'(.+)({|\[|\()+?')
+                #f = orth_pattern.sub('\g<1>', f)[0]
+                orth.text = f
+                gram_pattern = re.compile(r'{.+?}')
+                gram_match = gram_pattern.findall(f)
+                if gram_match:
+                    gramGrp = etree.SubElement(form, 'gramGrp')
+                    for g in gram_match:
+                        gram = etree.SubElement(gramGrp, 'gram')
+                        gram.text = g
+
+                usg_pattern = re.compile(r'\[.+?\]')
+                usg_match = usg_pattern.findall(f)
+                if usg_match:
+                    for u in usg_match:
+                        usg = etree.SubElement(form, 'usg')
+                        usg.text=u
+
+                note_pattern = re.compile(r'\(.+?\)')
+                note_match = note_pattern.findall(f)
+                if note_match:
+                    for n in note_match:
+                        note = etree.SubElement(form, 'note')
+                        note.text=n
+
+
+            for s in split_senses:
+                sense = etree.SubElement(entry, 'sense')
+                sense.text = s
+                '''
+                gram_pattern = re.compile(r'{.+?}')
+                gram_match = gram_pattern.findall(s)
+                if gram_match:
+                    gramGrp = etree.SubElement(sense, 'gramGrp')
+                    for g in gram_match:
+                        gram = etree.SubElement(gramGrp, 'gram')
+                        gram.text = g
+
+                usg_pattern = re.compile(r'\[.+?\]')
+                usg_match = usg_pattern.findall(s)
+                if usg_match:
+                    for u in usg_match:
+                        usg = etree.SubElement(sense, 'usg')
+                        usg.text=u
+
+                note_pattern = re.compile(r'\(.+?\)')
+                note_match = note_pattern.findall(s)
+                if note_match:
+                    for n in note_match:
+                        note = etree.SubElement(sense, 'note')
+                        note.text=n
+                '''
+
+        body.append(superentry)
+
+    et = etree.ElementTree(root)
+    return et
+
+
+et = transform_in_tei(input_output.deserialize('data/splitted_beolingus.pickle'))
+et.write('data/beolingus_tei_2.xml', pretty_print=True, xml_declaration=True, encoding='utf-8')
